2197c9f4e18b6243057711c48e9d86fda38e1e8c
ldgen <leondmikol@mail.ru>
2023-09-12 21:28:41 +0500

    форма телесного цвета и квадратная

--- MyNotepad/MyNotepad/Form1.Designer.cs
+++ MyNotepad/MyNotepad/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(88, 67);
+            this.button1.Location = new System.Drawing.Point(71, 88);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(146, 71);
             this.button1.TabIndex = 0;
@@ -45,8 +45,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gold;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
